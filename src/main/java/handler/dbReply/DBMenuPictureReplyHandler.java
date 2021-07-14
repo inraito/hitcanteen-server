ad034a59ed6085c1d2c36832853d13fa79cf2c31
inraito <939193803@qq.com>
2021-07-14 19:17:55 +0800

    fix a bug in PictureReplyHandler

--- src/main/java/handler/dbReply/DBMenuPictureReplyHandler.java
+++ src/main/java/handler/dbReply/DBMenuPictureReplyHandler.java
@@ -5,12 +5,43 @@ import io.vertx.core.AsyncResult;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.eventbus.Message;
+import io.vertx.core.file.AsyncFile;
 import io.vertx.core.file.FileSystem;
 import io.vertx.core.file.OpenOptions;
 import io.vertx.ext.web.RoutingContext;
 
 import java.util.ArrayList;
 
+class FileOpenCompletedHandler implements  Handler<AsyncResult<AsyncFile>>{
+    private final int count;
+    private final ArrayList<String> arrayList;
+    private final RoutingContext ctx;
+    private final FileSystem fs;
+    public FileOpenCompletedHandler(int i, ArrayList<String> arrayList, RoutingContext ctx, FileSystem fs){
+        this.count = i;
+        this.arrayList = arrayList;
+        this.ctx = ctx;
+        this.fs = fs;
+    }
+    @Override
+    public void handle(AsyncResult<AsyncFile> result) {
+        if (result.succeeded()) {
+            result.result().pipeTo(ctx.response(), ar -> {
+                if(ar.succeeded()){
+                    if(count<arrayList.size()-1)
+                        fs.open(arrayList.get(count+1), new OpenOptions(), new FileOpenCompletedHandler(count+1,arrayList, ctx, fs));
+                    else
+                        ctx.response().end();
+                }
+                else{
+                        ctx.response().end();
+                }
+            });
+        } else {
+                ctx.response().end();
+        }
+    }
+}
 
 public class DBMenuPictureReplyHandler implements Handler<AsyncResult<Message<DBMenuPictureReply>>> {
     private final RoutingContext ctx;
@@ -25,42 +56,7 @@ public class DBMenuPictureReplyHandler implements Handler<AsyncResult<Message<DB
         if(var1.succeeded()){
             FileSystem fs = vertx.fileSystem();
             ArrayList<String> arrayList = var1.result().body().getList();//get paths
-            for(int i=0;i<arrayList.size();i++) {//pipe all the pictures to response
-                if(i<arrayList.size()-1) {
-                    fs.open(arrayList.get(i), new OpenOptions(), result -> {
-                        if (result.succeeded()) {
-                            result.result().pipeTo(ctx.response(), ar -> {
-                                if(ar.succeeded()){ }
-                                else{
-                                    if(!ctx.response().ended())
-                                        ctx.response().end();
-                                }
-                            });
-                        } else {
-                            if(!ctx.response().ended())
-                                ctx.response().end();
-                        }
-                    });
-                }
-                else{
-                    fs.open(arrayList.get(i), new OpenOptions(), result -> {
-                        if (result.succeeded()) {
-                            result.result().pipeTo(ctx.response(), ar -> {
-                                if(ar.succeeded()){
-                                    ctx.response().end();
-                                }
-                                else{
-                                    if(!ctx.response().ended())
-                                        ctx.response().end();
-                                }
-                            });
-                        } else {
-                            if(!ctx.response().ended())
-                                ctx.response().end();
-                        }
-                    });
-                }
-            }
+            fs.open(arrayList.get(0), new OpenOptions(), new FileOpenCompletedHandler(0,arrayList, ctx, fs));
         }
         else{
             ctx.response().end("failure");
